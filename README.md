93da6a970beb86d414b4d3547b390237a7fcf722
Ivan Kurnosov <zerkms@zerkms.com>
2014-12-07 19:37:43 +1300

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Given a letter such as D, print a diamond of letters like:
+```
+   A
+  B B
+ C   C
+D     D
+ C   C
+  B B
+   A
+```
+
+See the tests for other examples.
+
+Inspired by https://github.com/giorgiosironi/diamond-kata
---
+++ diamond.hs
@@ -0,0 +1,28 @@
+import Data.List (elemIndex)
+
+letters = ['A'..'Z']
+
+height :: Char -> Maybe Int
+height c = elemIndex c letters
+
+lpad :: Int -> String
+lpad n = replicate n ' '
+
+mirror :: [a] -> [a]
+mirror xs = xs ++ (tail $ reverse xs)
+
+line :: Int -> Int -> String
+line h n = mirror (lpad (h - n) ++ [letters !! n] ++ lpad n)
+
+top_part :: Int -> [String]
+top_part h = [line h n ++ "\n" | n <- [0..h]]
+
+generate :: Int -> String
+generate h = concat $ mirror $ top_part h
+
+diamond :: Char -> String
+diamond c = case height c of
+        Just h -> generate h
+        _      -> "Not a valid input"
+
+main = putStrLn $ diamond 'D'
